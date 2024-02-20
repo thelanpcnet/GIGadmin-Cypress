c1425fbf48d6fe90412b52a226e7a2308ba00f65
thelanpcnet <thelanpcnet@gmail.com>
2024-02-20 14:23:18 +0700

    #1 - Setup environment and Login/Logout

---
+++ cypress/e2e/GIGadmin.login.cy.js
@@ -0,0 +1,29 @@
+describe('GIGadmin', () => {
+ 
+  it('login', () => {
+
+    // Enter GIGadmin portal
+    cy.viewport(1920,1080)
+    cy.clearAllCookies()
+    cy.visit(Cypress.env('URL'))
+    cy.wait(1000)
+    
+    // Start Login
+    cy.get('#login_email')
+      .click()
+      .type(Cypress.env('USERNAME'))
+    cy.wait(1000)
+    cy.get('#login_password')
+      .click()
+      .type(Cypress.env('PASSWORD'))
+    cy.wait(1000)
+    cy.get('.ant-btn')
+      .click()
+    
+    // Start Logout
+    cy.wait(2000)
+    cy.get('.ant-image-img').click()
+    cy.wait(1000)
+    cy.get('body > div:nth-child(7) > div > div > ul > li:nth-child(2) > span > li > span').click()
+  })
+})
